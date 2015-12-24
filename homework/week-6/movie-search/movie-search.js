690ddbedc1cee1dced344e2a8ecfcc243cdf0673
weeeitsmylinh <mylinh@My-Linhs-Air-3.att.net>
2015-12-23 17:42:14 -0800

    movie search hw

--- homework/week-6/movie-search/movie-search.js
+++ homework/week-6/movie-search/movie-search.js
@@ -1,33 +1,55 @@
-// This is what the data returning from IMDB will look like:
-var sampleResult = {
-  "Search": [
-    {
-      "Title": "Cool Runnings",
-      "Type": "movie",
-      "Year": "1993",
-      "imdbID": "tt0106611"
-    }
-  ]
-}
-
-// Attach an event listener to the form submit (using jQuery)
+// Handle the form submission: go to OMDB and get results
 $("#movie-search-form").submit(formSubmitted);
 
-// Handle the form submission: go to OMDB and get results
+//Gets IMDB data
 function formSubmitted(event) {
   event.preventDefault();
   var url = "http://omdbapi.com/?s=" + $("#query").val();
   $.get(url, resultsReceived);
 }
 
+//creates movie list
 function resultsReceived(results) {
-  // Try putting a debugger here and inspecting the results argument
-  // The array of movies lives inside results["Search"]
-  // See the sampleResult above for an example
 
-  // Access the array of movies:
-  results["Search"]
+  for (var i = 0; i < results["Search"].length; i++) {
 
-  // Access the first movie title
-  results["Search"][0]["Title"]
+      //check if result is a movie
+      var movieType = results["Search"][i]["Type"];
+
+      if (movieType == "movie") {
+
+       var movieTitle = results["Search"][i]["Title"];
+       var movieYear = results["Search"][i]["Year"];
+       var moviePoster = results["Search"][i]["Poster"];
+       var movieID = results["Search"][i]["imdbID"];
+
+       //make li
+       var li = document.createElement("li");
+       li.className = "movie";
+       var parent = document.querySelector("ul");
+       parent.appendChild(li);
+
+       //make poster
+       var poster = document.createElement("img");
+       li.appendChild(poster);
+       poster.setAttribute("src", moviePoster);
+       
+       //make title
+       var title = document.createElement("div");
+       title.className = "movie-title";
+       li.appendChild(title);
+       var titleLink = document.createElement('a');
+       titleLink.setAttribute("href","www.imdb.com/title/"+movieID);
+       title.appendChild(titleLink);
+       titleLink.textContent = movieTitle;
+
+       //make release date
+       var releaseDate = document.createElement("div");
+       releaseDate.className = "movie-release-date";
+       li.appendChild(releaseDate);
+       releaseDate.textContent = movieYear;
+
+
+     }
+   }
 }
